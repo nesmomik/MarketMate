a4c25607175fdf7aa53007f3bd3966c222b100a7
Michael Thomsen <nesmomik@gmail.com>
2026-02-26 21:49:54 +0100

    feat: add global block list listener rule, point health check to /health

--- backend/infrastructure/main.tf
+++ backend/infrastructure/main.tf
@@ -233,16 +233,101 @@ resource "aws_lb_listener_rule" "instances" {
   }
 }
 
+# block list part 1
+resource "aws_lb_listener_rule" "block_list_1" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 1
+
+  action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Not Found"
+      status_code  = "404"
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = [
+        "*.php*",
+        "*/vendor/*",
+        "*/.env*",
+        "*/.git*",
+        "/wp-admin*"
+      ]
+    }
+  }
+}
+
+# block list part 2
+resource "aws_lb_listener_rule" "block_list_1" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 2
+
+  action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Not Found"
+      status_code  = "404"
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = [
+        "/wp-login*",
+        "*/config.php",
+        "/cgi-bin/*",
+        "*/.aws/*",
+        "*/.ssh/*"
+      ]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "global_block_list" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 1 
+
+  action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Not Found"
+      status_code  = "404" 
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = [
+        "*.php*",       
+        "*/vendor/*",   
+        "*/.env*",      
+        "*/.git*",      
+        "/wp-admin*",   
+        "/wp-login*",   
+        "*/config.php", 
+        "/cgi-bin/*",   
+        "*/.aws/*",     
+        "*/.ssh/*"      
+      ]
+    }
+  }
+}
+
 resource "aws_lb_target_group" "web_app_tg" {
-  name     = "example-target-group"
+  name     = "web-app-target-group"
   port     = 5000
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default_vpc.id
   health_check {
-    path                = "/"
+    path                = "/health"
     protocol            = "HTTP"
     matcher             = "200"
-    interval            = 15
+    interval            = 30
     timeout             = 3
     healthy_threshold   = 2
     unhealthy_threshold = 2
